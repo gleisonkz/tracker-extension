1f69cb716e74213a838c9bb7c1c1691ff5610274
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-01 15:31:52 -0300

    ✨ feat: notificação para novos publicações

--- src/background.ts
+++ src/background.ts
@@ -1,31 +1,39 @@
 const audio = new Audio("./assets/audio/cash.mp3");
-audio.play();
+const date = new Date();
+let previousValue: number;
 
-const newURL = "https://www.climaaovivo.com.br/mg/belo-horizonte/regiao-central";
+const [day, month, year] = [
+  date.getDate().toString().padStart(2, "0"),
+  (date.getMonth() + 1).toString().padStart(2, "0"),
+  date.getFullYear(),
+];
 
-let activeTab: chrome.tabs.Tab = null;
-let updated = false;
+const browserURL = `https://www.bcb.gov.br/estabilidadefinanceira/buscanormas?conteudo=DEPIN%20realiza%C3%A7%C3%A3o&dataInicioBusca=${day}%2F${month}%2F${year}&dataFimBusca=${day}%2F${month}%2F${year}&tipoDocumento=Comunicado`;
+const apiURL = `https://www.bcb.gov.br/api/search/app/normativos/buscanormativos?querytext=ContentType:normativo%20AND%20contentSource:normativos%20AND%20DEPIN%20realiza%C3%A7%C3%A3o%20AND%20TipodoNormativoOWSCHCS=%22Comunicado%22&rowlimit=15&startrow=0&sortlist=Data1OWSDATE:descending&refinementfilters=Data:range(datetime(${year}-${month}-${day}),datetime(${year}-${month}-${day}T23:59:59))`;
 
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.create({ url: newURL }, (tab) => {
-    activeTab = tab;
-    chrome.tabs.executeScript(null, { file: "./content.js" });
-  });
-});
+setTimeout(getData, 5000);
+setInterval(getData, 60000);
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.indexOf(newURL) > -1 && changeInfo.status === "complete" && updated) {
-    console.log("CHANGE");
-    chrome.tabs.executeScript(activeTab.id, { file: "./content.js" });
-  }
-});
+async function getData() {
+  const data = await fetch(apiURL).then((response) => response.json());
+  const { TotalRows: totalRows } = data;
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "storage-change") {
-    chrome.storage.local.get("last-data", (value) => {
-      console.log(value);
-      updated = true;
-      chrome.tabs.reload(activeTab.id);
+  if (totalRows && previousValue !== totalRows) {
+    previousValue = totalRows;
+
+    chrome.notifications.create("notification", {
+      title: "Publicação nova disponível",
+      message: "How great it is!",
+      iconUrl: "./assets/image/robot-face.png",
+      type: "basic",
     });
+
+    chrome.notifications.onClicked.addListener((notificationID: string) => {
+      if (notificationID === "notification") {
+        chrome.tabs.create({ url: browserURL });
+      }
+    });
+
+    audio.play();
   }
-});
+}
